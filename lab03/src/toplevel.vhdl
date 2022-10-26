99d8deb62a43a31cc40b5a18c857e6cd50e9ad23
Simon Thür <thuer.simon@hotmail.com>
2022-10-26 08:20:42 +0200

    create .vhdl files

---
+++ lab03/src/toplevel.vhdl
@@ -0,0 +1,17 @@
+--=============================================================================
+-- @file toplevel.vhdl
+-- @author Simon Thür
+--=============================================================================
+-- Standard library
+library ieee;
+-- Standard packages
+use ieee.std_logic_1164.all;
+
+--=============================================================================
+--
+-- toplevel
+--
+-- @brief This file specifies the toplevel for the the keylock (lab 3)
+--
+--=============================================================================
+entity toplevel is
