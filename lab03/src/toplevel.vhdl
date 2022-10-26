c07e8fea14c5d2fd3c963533dde4a2a70f10a6e5
Simon Thür <thuer.simon@hotmail.com>
2022-10-26 10:15:15 +0200

    toplevel begin

--- lab03/src/toplevel.vhdl
+++ lab03/src/toplevel.vhdl
@@ -14,3 +14,50 @@ use ieee.std_logic_1164.all;
 -- @brief This file specifies the toplevel for the the keylock (lab 3)
 --
 --=============================================================================
+
+entity toplevel is
+    port (
+        CLKxCI : in std_logic;
+        RSTxRI : in std_logic;
+        Push0xSI : in std_logic;
+        Push1xSI : in std_logic;
+        Push2xSI : in std_logic;
+        Push3xSI : in std_logic;
+        RLEDxSO : out std_logic;
+        GLEDxSO : out std_logic
+    );
+end toplevel;
+
+
+architecture rtl of toplevel is
+
+  COMPONENT KeyLock
+    PORT (
+        CLKxCI : in std_logic;
+        RSTxRI : in std_logic;
+
+        KeyValidxSI : in std_logic;
+        KeyxDI  : in unsigned(3 downto 0);
+
+        GLEDxSO : out std_logic;
+        RLEDxSO : out std_logic
+    );
+  END COMPONENT;
+
+  
+  
+  BEGIN
+  key_lock : KeyLock
+  PORT MAP(
+      CLKxCI => CLKxCI,
+      RSTxRI => RSTxRI,
+      
+      KeyValidxSI => KeyValidxSI,
+      KeyxDI  => KeyxDI ,
+      
+      GLEDxSO => GLEDxSO,
+      RLEDxSO => RLEDxSO,
+      )
+
+
+end rtl;
