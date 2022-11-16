51dba56d8879ac92a402c77e3aecba41d3be7383
Simon Thür <thuer.simon@hotmail.com>
2022-11-16 11:05:28 +0100

    add testbench file

---
+++ lab04/project_1.srcs/sources_1/new/arbiter_tb.vhdl
@@ -0,0 +1,26 @@
+----------------------------------------------------------------------------------
+-- Company: EPFL
+-- Engineer: Simon J. Thür
+-- 
+-- Create Date: 16.11.2022 11:03:36
+-- Design Name: TEST BENCH for arbiter
+-- Module Name: arbiter_tb - Behavioral
+-- Project Name: ARBITER
+-- Target Devices: PINQ
+-- Tool Versions: idk
+-- Description: tb
+-- 
+-- Dependencies: idk
+-- 
+-- Revision:0.01
+-- Revision 0.01 - File Created
+-- Additional Comments: NONE
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
+
+
