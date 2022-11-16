0f844d224c475a2aacb6da76418a9b00205a2b92
Simon Thür <thuer.simon@hotmail.com>
2022-11-16 09:29:56 +0100

    create file

---
+++ lab04/project_1.srcs/sources_1/new/arbiter.vhdl
@@ -0,0 +1,33 @@
+----------------------------------------------------------------------------------
+-- Company: EPFL
+-- Engineer: Simon J. Thür
+-- 
+-- Create Date: 16.11.2022 09:25:28
+-- Design Name: ARBITER
+-- Module Name: arbiter - Behavioral
+-- Project Name: STUFF
+-- Target Devices: PINQ
+-- Tool Versions: IDK
+-- Description: ARBITER FOR KEYS
+-- 
+-- Dependencies: NONE
+-- 
+-- Revision: 0.01
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
