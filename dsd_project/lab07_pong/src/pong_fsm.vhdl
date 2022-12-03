2cc4063736eaff6fa4383c9e4322d07588ab4eac
Simon Thür <thuer.simon@hotmail.com>
2022-12-03 22:58:03 +0100

    maybe fix start condition?

--- dsd_project/lab07_pong/src/pong_fsm.vhdl
+++ dsd_project/lab07_pong/src/pong_fsm.vhdl
@@ -185,10 +185,9 @@ BEGIN
 
   -- not very energy efficient but such is life
   BallPosYSetValxD <= "00"& VgaYxDI(COORD_BW-1 DOWNTO 2)+1;  -- upper quarter of screen
-  BallPosXSetValxD <= ("00"&VgaXxDI(COORD_BW-1 DOWNTO 2))+(HS_DISPLAY/2);  --middle half of screen
-
+  BallPosXSetValxD <= ("00"&VgaXxDI(COORD_BW-1 DOWNTO 2))+(HS_DISPLAY/4);  --middle half of screen TODO ACTUALLY DO IT PROPERLY
   --set?
-  SetCntrs <= '1' WHEN GameActivexSN = '1' AND GameActivexSP = '0' ELSE
+  SetCntrs         <= '1' WHEN GameActivexSN = '1' AND GameActivexSP = '0' ELSE
               '0';
   cntBarDirxS <= LeftxSI;               -- xor is handled by countenable
   -- direction signals
